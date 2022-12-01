7a92d9568fe27c643aa962ee1ec82ef1b7bdf6ad
kchandrabhatta <kchandrabhatta@gmail.com>
2022-11-30 19:41:37 -0800

    feature: complete urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,34 @@
-
-# Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# temp add Cats class
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}"
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+
+
+# Create your views here.
 def index(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html', { 'cats': cats })
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request): 
+    return render(request,'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
